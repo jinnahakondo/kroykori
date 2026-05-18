880832008728003ca94a411e464e2eebd3065b43
mdjinnahakondo <mdjinnahakondo@gmail.com>
2026-05-18 21:25:16 +0600

    updated user schema (added provider feild

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,6 +3,11 @@ import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
     {
+        _id: mongoose.Schema.Types.ObjectId,
+        provider: {
+            type: String,
+            enum: ["credential", "google",],
+        },
         role: {
             type: String,
             enum: ["user", "admin"],
